682f6ea358a08ac7dd60324d0b72244aac87a625
Igor Popov <ihptru@gmail.com>
2012-01-08 16:54:04 +0400

    fix thirdline ( kick, topic, nick, op, join ) seems deop does not work somehow

--- orabot.pm
+++ orabot.pm
@@ -10,11 +10,12 @@ sub new
 {
     my ($type, %args) = @_;
     my $timestamp = '\d+-[\d\w]+-\d+[ T](\d+):\d+:\d+';
+    my $thirdtimestamp = '\d+-[\d\w]+-\d+[ T](\d+):(\d+):\d+';
     my $self = {
         cfg => $args{cfg},
         normalline => '^\[?'.$timestamp.']? <(\S+)> (.*)',
         actionline => '^\[?'.$timestamp.']? \* (\S+) (.*)',
-        thirdline  => '\[?'.$timestamp.']? \*\*\* (\S+) (\S+) (\S+) (\S+) ?(.*)?',
+        thirdline  => '^\[?'.$thirdtimestamp.']? \*\*\* (\S+) (\S+) (\S+) (\S+) ?(.*)?',
     };
 
     bless($self, $type);
@@ -55,26 +56,6 @@ sub actionline
     }
 }
 
-# Parses the 'third' line - (the third line is everything else, like
-# topic changes, mode changes, kicks, etc.)
-# thirdline() has to return a hash with the following keys, for
-# every format:
-#   hour            - the hour we're in (for timestamp logging)
-#   min             - the minute we're in (for timestamp logging)
-#   nick            - the nick
-#   kicker          - the nick which kicked somebody (if any)
-#   newtopic        - the new topic (if any)
-#   newmode         - deops or ops, must be '+o' or '-o', or '+ooo'
-#   newjoin         - a new nick which has joined the channel
-#   newnick         - a person has changed nick and this is the new nick
-# 
-# It should return a hash with the following (for formatting lines in html)
-#
-#   kicktext        - the kick reason (if any)
-#   modechanges     - data of the mode change ('Nick' in '+o Nick')
-#
-# The hash may also have a "repeated" key indicating the number of times
-# the line was repeated. (Used by eggdrops log for example.)
 sub thirdline
 {
     my ($self, $line, $lines) = @_;
@@ -86,8 +67,28 @@ sub thirdline
         $hash{min}  = $2;
         $hash{nick} = $3;
 
-        # Format-specific stuff goes here.
-
+	if (($4.$5) eq 'waskicked') {
+            $hash{kicker} = $7;
+            ($hash{kicktext} = $hash{kicker}) =~ s/\S+\s*//;
+            $hash{kicker} =~ s/\s.*//;
+            $hash{kicktext} =~ s/^\((.*)\)$/$1/;
+        } elsif (($4.$5) eq 'changestopic') {
+            $hash{newtopic} = $7;
+        } elsif (($4.$5) eq 'giveschannel') {
+	    $hash{newmode} = "+o";
+            $hash{modechanges} = $7;
+	    $hash{modechanges} =~ s/^status to\s*//;
+	} elsif (($5.$6) eq 'removeschannel') {
+	    $hash{newmode} = "-o";
+	    $hash{modechanges} = $7;
+	    $hash{modechanges} =~ s/^status from\s*//;
+        } elsif (($5.$6) eq 'hasjoined') {
+            $hash{newjoin} = $3;
+        } elsif (($5.$6) eq 'nowknown') {
+            $hash{newnick} = $7;
+            $hash{newnick} =~ s/^as\s*//;
+        }
+		
         return \%hash;
 
     } else {
